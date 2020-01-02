0fead0697ea1498fb5a4a42c203982e093b1b8a5
mysubsnewslivecom <my.subs.news@live.com>
2020-01-02 21:15:46 +0000

    02-Jan-2020 21:15:09

---
+++ kafka.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+if [ -f /home/kishen/Workspace/config/env.sh ]; then
+    . /home/kishen/Workspace/config/env.sh
+    clear
+    ScriptDetails
+else
+        printf "[$(date "+%d-%b-%Y %H:%M:%S") #ERROR] env.sh does not exists\n" 
+    exit 1
+fi
+
+ARGS="--broker-list localhost:9092 --topic com.test"
+
+msg="Test Message"
+
+log "Sending ${msg}" INFO
+
+log "${msg}" INFO| ${KAFKA_BIN}/kafka-console-producer.sh ${ARGS} >/dev/null 2>&1
+
+[ $? -ne 0 ] && log "Failed" 
+unset msg
+
+
+
+#${KAFKA_BIN}/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic com.test --from-beginning
+
+
+
+EndScript
