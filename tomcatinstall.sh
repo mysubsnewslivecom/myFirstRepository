63d4477df7a8878303d26fa1e1fe2a5595ee3fc9
mysubsnewslivecom <my.subs.news@live.com>
2020-01-02 21:09:54 +0000

    02-Jan-2020 21:09:34

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 clear
 
-. /home/kishen/Workspace/config/env.sh 
-ScriptDetails
+if [ -f /etc/profile.d/env.sh ]; then
+    . /etc/profile.d/env.sh
+else
+        printf "[$(date "+%d-%b-%Y %H:%M:%S") #ERROR] env.sh does not exists\n" 
+    exit 1
+fi 
+#ScriptDetails
 
 usage(){
 
@@ -12,19 +17,6 @@ usage(){
 
 
 }
-
-usage $#
-
-file="$1"
-directory="$2"
-installdirectory="/opt/${directory}"
-#filename=$(basename -- "$file")
-#extension="${filename##*.*}"
-#filename="${filename%.*}"
-
-#echo $filename
-#echo $extension
-
 status (){
     message="$1"
     code="$2"
@@ -32,43 +24,111 @@ status (){
     unset message
     unset code
 }
+usage $#
 
-echo $file|grep "."
+file="$1"
+directory="$2"
+installdirectory="/opt/${directory}"
 
-log "${installdirectory}"
+_self="${0##*.}"
+#echo "$_self is called"
 
-if [ -f /home/kishen/Downloads/${file} ]; then
-    log "File exists" 
-else
-    log "File does not exists" ERROR
-    exit 2
-fi
 
-if [ -d ${installdirectory} ]; then
-    log "Directory ${installdirectory} exists "
-else
-    log "Directory does not exists"
-    sudo mkdir ${installdirectory} -p
-    sudo chown -R ${USER}:common ${installdirectory}
-    ls -ld ${installdirectory}
-fi
+
+
+
+
+
+
+
+
+
+fileDetails(){
+
+
+    if [ -f ${file} ]; then
+        log "File exists" 
+    else
+        log "File ${file} does not exists" ERROR
+        #exit 2
+    fi
+
+    filename=$(basename -- "${file}")
+    extension="${filename#*.}"
+    case "$filename" in
+        *.tar.bz2) 
+            subDir=`echo "${filename}"|sed 's/.tar.bz2//g'` 
+            ;;
+        *.bz2)
+            subDir=`echo "${filename}"|sed 's/.bz2//g'`
+            ;;
+        *.tar.gz) 
+            subDir=`echo "${filename}"|sed 's/.tar.gz//g'`
+#            cmd="tar -xzf ${installdirectory}/${filename} -C ${installdirectory}/${subDir}/"
+            cmd="tar -xzf ${installdirectory}/${filename} "
+            ;;
+        *.tgz)
+            subDir=`echo "${filename}"|sed 's/.tgz//g'`
+            cmd="tar -xzf ${installdirectory}/${filename} "
+            ;;
+        *.gz)      
+            subDir=`echo "${filename}"|sed 's/.gz//g'`
+            cmd="gunzip -c ${installdirectory}/${filename} > ${installdirectory}/${subDir}/"
+            ;;
+        *.zip)           
+            subDir=`echo "${filename}"|sed 's/.zip//g'`
+            cmd="unzip  ${installdirectory}/${filename} -d ${installdirectory}/${subDir}/"
+            ;;
+        *.7z)  
+            subDir=`echo "${filename}"|sed 's/.7z//g'`
+            ;;
+        *)
+            echo "invalid extension" 
+            exit 1            
+            ;;
+    esac
+
+    log "File: ${file}"
+    log "Install Directory: ${installdirectory}"
+    log "DirectoryName: ${subDir}"
+    log "Command: ${cmd}"
+
+    if [ -d ${installdirectory} ]; then
+        log "Directory ${installdirectory} exists "
+    else
+        log "Directory ${installdirectory} does not exists"
+        sudo mkdir ${installdirectory}/ -p
+        sudo chown -R ${USER}:common ${installdirectory}/
+        ls -ld ${installdirectory}
+    fi
+
+    start
+
+}
+
 
 start(){
-    cp  /home/kishen/Downloads/${file} ${installdirectory}
+
+#    local CMD=$1
+    cp  -ipv ${file} ${installdirectory}
     status "Copy file to destination" $?
     chmod 755 ${installdirectory}/${file}
     status "Changing permisssion" $?
     log "`ls -lrt ${installdirectory}/${file}`"
     cd ${installdirectory}
-    tar -xf ${file}
+    ${cmd}
     status "Untaring ${file}" $?
-    ln -sf ${file} latest
-    status "Symlink ${file}" $?
-    chown -R ${USER}:common ${installdirectory}/${file}
+    ln -sf ${subDir} latest
+    status "Symlink ${subDir}" $?
+    sudo chown -R ${USER}:common ${installdirectory}/${subDir}
     status "Changing owner" $?
 }
 
-start
+
+
+
+
+fileDetails
 
 EndScript
 
