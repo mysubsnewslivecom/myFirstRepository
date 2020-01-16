61df79b75541cbbbc45333bbc597285d0caf8c0f
mysubsnewslivecom <my.subs.news@live.com>
2020-01-16 20:20:36 +0000

    updated

--- status.sh
+++ status.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 clear
+
 if [ -f /etc/profile.d/env.sh ]; then
     . /etc/profile.d/env.sh
 else
@@ -21,12 +8,18 @@ else
     exit 1
 fi
 
-[ $# -eq 0 ] && printf "[$(date "+%d-%b-%Y %H:%M:%S") #ERROR] Usage: $0 appln\n" 
+[ $# -eq 0 ] && log "Usage: $0 appln" ERROR 
 
+param=$1
 
-PATTERN="\$PID_"${1^^}
+PATTERN="\$PID_"${param^^}
 application=`eval echo ${PATTERN}|cut -d'|' -f1`
 PATTERN=`eval echo ${PATTERN}|cut -d'|' -f2`
+
+if [ -z "${PATTERN}" -o -z "${application}" ] ; then
+    log "${param} not found" ERROR
+fi
+
 
 checkPID(){
     local PID="${1}"
